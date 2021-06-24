8171e46d28f9058c1bdf2d8b1e48f66b7cac5fb5
kleitonalvesrb <kleiton.a.batista@gmail.com>
2021-06-24 19:11:22 -0300

    alterando a porta

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require('./src/app');
 
 const port = 3300;
 
-app.listen(port, () =>{
+app.listen(process.env.PORT || 3300, () =>{
     console.log('Aplicação executando na porta ',port);
 });

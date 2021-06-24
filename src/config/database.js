dc92bcc417b6075e7779694fcaf55342c79c4718
kleitonalvesrb <kleiton.a.batista@gmail.com>
2021-06-24 19:51:03 -0300

    alterando dados de banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,10 @@ dotenv.config();
 
 const pool = new Pool({
     // connectionString : process.env.DATABASE_URL
-    connectionString : "postgres://embyjrlaecqbwz:f307a929283d81ed859f7a1cb7dc39ff58b451ff33ac8e65195b977e0d34a212@ec2-35-169-188-58.compute-1.amazonaws.com:5432/daucppg3amgrib"
+    connectionString : "postgres://embyjrlaecqbwz:f307a929283d81ed859f7a1cb7dc39ff58b451ff33ac8e65195b977e0d34a212@ec2-35-169-188-58.compute-1.amazonaws.com:5432/daucppg3amgrib",
+    ssl: {
+        rejectUnauthorized: false
+      }
 });
 
 pool.on('connect', () =>{
